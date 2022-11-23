5baab843184d94d0bba4c8b240c10d8f7597c930
Taylor Wagstaff <taylorwagstaff@gmail.com>
2022-11-23 14:07:18 +1300

    finished new array input for manipluation of data

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
 import Head from 'next/head'
-import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 import { useEffect, useState } from 'react'
 
 import { WORDS } from '../components/wordlist'
 
+
 const getRandomWords = () => {
   return WORDS[Math.floor(Math.random() * WORDS.length)]
 }
 
+// To do:
+// make only letters in input
+// check each word with api from parts of array after each handlechange
+// change colour if word is correct
+// conditions for letter going down and across then...
+// add points based on letter values to a point summary
+// change colour if letters go in certain directions
+// add directions for gameplay
+// styling
+
+
 export default function Home() {
-  // const word = ['P', 'E', 'O', 'P', 'L', 'E']
+ 
   const [randomWord, setRandomWord] = useState([])
   const [letter, setLetter] = useState([
     '0',
@@ -56,13 +68,77 @@ export default function Home() {
     // shuffle array and pick random
 
     const newWord = getRandomWords()
-    // const splitWord = newWord.split('')
-    //removing postion from array
-    const remove = [0, 6, 12, 18, 24, 30]
+    const splitWord = newWord.split('')
 
-   
-    
     setRandomWord(newWord)
+    setLetter([
+      splitWord[0],
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      splitWord[1],
+      '7',
+      '8',
+      '9',
+      '10',
+      '11',
+      splitWord[2],
+      '13',
+      '14',
+      '15',
+      '16',
+      '17',
+      splitWord[3],
+      '19',
+      '20',
+      '21',
+      '22',
+      '23',
+      splitWord[4],
+      '25',
+      '26',
+      '27',
+      '28',
+      '29',
+      splitWord[5],
+      '31',
+      '32',
+      '33',
+      '34',
+      '35',
+    ])
+    //removing postion from array
+
+    //  replace items with newWord at postion remove
+
+    // const replacedSet = letter.map((letters, index) => {
+    //   const splitWord = newWord.split('')
+    //   const remove = [0, 6, 12, 18, 24, 30]
+
+    //   console.log(`this is letters: ${splitWord}`)
+    //   console.log(`this is index of each letter: ${index}`)
+
+    //   if ((index === 0, 6, 12, 18, 24, 30)) {
+    //     console.log('conditions met')
+
+    //     const newLetterList = [
+    //       // Items before the insertion point:
+    //       ...letter.slice(0, index),
+    //       // New item:
+    //       splitWord,
+    //       // Items after the insertion point:
+    //       ...letter.slice(index),
+    //     ]
+
+    //     setLetter(newLetterList)
+    //   } else {
+    //     // The rest haven't changed
+    //     return letters
+    //   }
+    // })
+    // console.log(letter)
   }
 
   // Add letter
@@ -76,6 +152,8 @@ export default function Home() {
       // new array insert at its position
       setLetter([...letter.slice(0, param1), result, ...letter.slice(param1)])
       console.log(param1)
+
+      // check if word matches after each state change
     }
   }
 
@@ -115,7 +193,6 @@ export default function Home() {
               id="letter"
               type="text"
               maxLength={1}
-              pattern="[A-Za-z]"
               onChange={(event) => handleChange(event, 1)}
               onKeyDown={(event) => onKeyDown(event, 1)}
             ></input>
